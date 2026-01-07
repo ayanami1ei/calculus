db09052b5de0f4d7e4d3ad022ff55df4d04b15d0
ayanami-linux <cwb511@outlook.com>
2026-01-07 18:44:52 +0800

    fix bugs in derivative part

--- monomial/mod.rs
+++
@@ -1,7 +0,0 @@
-mod implement;
-
-pub struct Monomial {
-    variable: String,
-    coefficient: f32,
-    exponent: f32,
-}
--- polynomial/mod.rs
+++
@@ -1,7 +0,0 @@
-use crate::monomial::Monomial;
-
-pub mod implement;
-
-pub struct Polynomial{
-    monomials:Vec<Monomial>,
-}
